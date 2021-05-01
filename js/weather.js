9ac31db6c986f639881be6ab36d5cf194e7fe8b9
danchew90 <danchew90@naver.com>
2021-05-01 18:12:38 +0900

    update

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ const API_KEY = "36b289e5b9496ef9299f403a3e0a1c44";
 const COORDS = "coords";
 
 function getWeather(lat,lng){
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
     ).then(function(response){
        return response.json()
     }).then(function(json){
